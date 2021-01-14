15decaf3e9dd5463017a7bb0db4f0977a1555757
hdud826 <74641925+hdud826@users.noreply.github.com>
2021-01-14 12:28:57 +0900

    Create CSS.cs

---
+++ CSS.cs
@@ -0,0 +1,45 @@
+div.dataList{
+    display: flex;
+    height: 462px;
+}
+div.circleDiv{
+    display: inline-block;
+    /* background-color: cadetblue; */
+    width: 165px;
+    height: 100%;
+    text-align: center;
+}
+div.circle {
+    border-radius: 50%;
+    width: 58px;
+    height: 58px;
+    margin: 40px 50px;
+    background: white;
+    text-align: center;
+    line-height: 56px;
+    border: solid 1px;
+    border-color: black;
+}
+div.circle.high.selected{
+    background-color: blue;
+    color: white;
+}
+div.circle.normal.selected{
+    background-color: green;
+    color: white;
+}
+div.circle.low.selected{
+    background-color: red;
+    color: white;
+}
+div.lineDiv{
+    display: inline-block;
+    margin: 0 -56px 0 -60px;
+    /* background-color: khaki; */
+    width: 100px;
+    height: 441px;
+}
+svg{
+    width: 100px;
+    height: 90%;
+}
